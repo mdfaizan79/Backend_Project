428e1bec2f53e8f6e2dae59fa6a33b94a26af078
mdfaizan79 <[mdfaizan7707@gmail.com]>
2025-03-12 15:15:36 +0530

    Connection Done

--- Project01/src/app.js
+++ Project01/src/app.js
@@ -13,6 +13,17 @@ app.use(express.json({limit: "16kb"}))
 
 app.use(express.urlencoded({extended :true,limit :"16kb"})) //url me jo extra space ko  "%"  me convert wala jo manage krrega uss k liye
 
-app.use(express.static("public")) //
+app.use(express.static("public")) 
+app.use(cookieParser())
+
+//routes import
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+
+app.use("/api/v1/users",userRouter)
+//eg
+//http://localhost:8000//api/v1/users/register
 
 export {app}
---
+++ Project01/src/utils/fileupload.js
@@ -0,0 +1,48 @@
+import {v2 as cloudinary } from "cloudinary" ;
+import fs from "fs" ;
+
+ // Configuration
+ cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_CLOUD_KEY, 
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try{
+    if(!localFilePath) return null;
+
+    //Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath,{
+        resource_type: "auto"
+    })
+    
+    //file has uploaded sucessfully
+    console.log("File is Uploaded on Cloudinary ", response.url);
+    return response;
+    }
+    catch(error){
+    fs.unlinkSync(localFilePath) //remove the locally saved temp file as uploaded operation got failed
+    return null;
+    }
+}
+
+
+// cloudinary.v2.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg"),
+// { public_id: 'shoes'},
+// function(error,result) 
+
+
+
+// Upload an image
+const uploadResult = await cloudinary.uploader
+.upload(
+    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+)
+.catch((error) => {
+    console.log(error);
+});
+
+console.log(uploadResult);
